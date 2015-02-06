c25c5043ddcacacc4ddb1420c9b1afcc5ec4135b
Henrik Hofmeister <henrik@newdawn.dk>
2015-02-06 13:34:49 +0100

    Minor fixes

--- modules/webi-mongo/src/main/java/com/vonhof/webi/mongo/dto/BasicDTO.java
+++ modules/webi-mongo/src/main/java/com/vonhof/webi/mongo/dto/BasicDTO.java
@@ -24,6 +24,23 @@ public class BasicDTO {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        BasicDTO basicDTO = (BasicDTO) o;
+
+        if (id != null ? !id.equals(basicDTO.id) : basicDTO.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
     public String toString() {
         return String.format("%s[%s]",this.getClass().getSimpleName(),this.getId());
     }
